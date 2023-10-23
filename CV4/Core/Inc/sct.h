807b56cf9861a96e202572e02ad000d1b2c005d1
AKos <alzbeta.kostelanska@gmail.com>
2023-10-23 23:49:09 +0200

    cv4 - na otestovanie

---
+++ CV4/Core/Inc/sct.h
@@ -0,0 +1,18 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 11, 2023
+ *      Author: xkoste13
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+
+void sct_led(uint32_t value);
+void sct_init();
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* SCT_H_ */
+
